6bc5334b0786be24bf57cc77de5640890ee3fa5a
Jesse Lintunen <jesse.lintunen13@gmail.com>
2020-11-10 14:36:11 +0200

    valmis

--- main.js
+++ main.js
@@ -32,105 +32,105 @@ function formValidation()
 } 
 
 
-function userid_validation(uid,mx,my) {
-    var uid_len = uid.value.length;
+function userid_validation(uid, minLength,maxLength) {
     
-    if (uid_len == 0 || uid_len >= my || uid_len < mx) {
-        alert("Käyttäjä ID ei pitäisi olla tyhjä / pituus välillä "+mx+" to "+my);
-            uid.focus();
-                return false;}
-                    return true;}
+var uid_len = uid.value.length;
+    
+    if (uid_len >= maxLength || uid_len < minLength) {
+        alert("Käyttäjä ID ei pitäisi olla tyhjä / pituus välillä "+minLength+" to "+maxLength);
+        uid.focus();
+        return false;
+    }
+    
+    return true;
+}
                     
                     
-function passid_validation(passid,mx,my){
-var passid_len = passid.value.length;
+function passid_validation(passid,mx,my) {
+
+    var passid_len = passid.value.length;
 
     if (passid_len == 0 ||passid_len >= my || passid_len < mx){
-        alert("Salasanan ei pitäisi olla tyhjä / pituus välillä "+mx+" to "+my);
-            passid.focus();
-                return false;}
-                    return true; }
+    alert("Salasanan ei pitäisi olla tyhjä / pituus välillä "+mx+" to "+my);
+    passid.focus();
+    return false;}
+    return true; }
 
 function allLetter(uname) { 
-var letters = /^[A-Za-z]+$/;
+
+    var letters = /^[A-Za-z]+$/;
 
     if(uname.value.match(letters)) {
-        return true; }
-            else{
-                alert('Username must have alphabet characters only');
-                    uname.focus();
-                        return false; }
+    return true; }
+    else{
+    alert('Käyttäjänimessä täytyy olla vain kirjaimia');
+    uname.focus();
+    return false; }
 }
-function alphanumeric(uadd){ 
+
+function alphanumeric(uadd)
+{ 
 var letters = /^[0-9a-zA-Z]+$/;
 
-    if(uadd.value.match(letters)){
-        return true;}
-            else{
-                alert('User address must have alphanumeric characters only');
-                    uadd.focus();
-                        return false;
+    if(uadd.value.match(letters))
+    {
+    return true;
+    }
+    else
+    {
+    alert('Käyttäjä osoitteessa täytyy olla vain numeroita');
+    uadd.focus();
+    return false;
 }
 }
-function countryselect(ucountry)
+function countryselect(ucountry){
+    
+        if(ucountry.value == "Default")
 {
-if(ucountry.value == "Default")
-{
-alert('Select your country from the list');
+alert('Valitse maasi listalta');
 ucountry.focus();
 return false;
-}
-else
-{
-return true;
-}
-}
-function allnumeric(uzip)
-{ 
-var numbers = /^[0-9]+$/;
-if(uzip.value.match(numbers))
-{
-return true;
-}
-else
-    {
-alert('ZIP code must have numeric characters only');
-uzip.focus();
-return false;
-}
-}
-function ValidateEmail(uemail)
-{
-var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-if(uemail.value.match(mailformat))
-{
-return true;
-}
-else
-{
-alert("You have entered an invalid email address!");
-uemail.focus();
-return false;
-}
+        }
+        else{
+        return true;}
+        }
+
+function allnumeric(uzip){ 
+
+    var numbers = /^[0-9]+$/;
+    
+    if(uzip.value.match(numbers)){
+    return true;}
+    else{
+    alert('Postinumerossa täytyy olla voin numeroita');
+    uzip.focus();
+    return false;}}
+
+function ValidateEmail(uemail){
+    
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    
+    if(uemail.value.match(mailformat)){
+    return true;}
+    else{
+    alert("Olet syöttänyt käyttämättömän sähköpostin!");
+    uemail.focus();
+    return false;}
+    
 } function validsex(umsex,ufsex)
 {
 x=0;
 
-if(umsex.checked) 
-{
-x++;
-} if(ufsex.checked)
-{
-x++; 
-}
-if(x==0)
-{
-alert('Select Male/Female');
-umsex.focus();
-return false;
-}
-else
-{
+        if(umsex.checked) {
+        x++;
+        } if(ufsex.checked){
+        x++; }
+        if(x==0) {
+        alert('Select Male/Female');
+        umsex.focus();
+        return false; }
+        else {
+
 alert('Form Succesfully Submitted');
 window.location.reload()
 return true;
